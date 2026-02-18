79ecfadd14b1f8266685602623516810f7e54a27
MD SANAULLAH <alamsanaullah57@gmail.com>
2026-02-18 13:05:03 +0530

    Add comments for using bge-m3 model

--- projects/embedd_array_of_strings.py
+++ projects/embedd_array_of_strings.py
@@ -1,5 +1,7 @@
 import request
 
+# using the model bge-m3 of Ollama
+# make sure to install ollama and load the bge-m3 model locally
 def create_embeddings(list_text):
   res = requests.post("http://localhost:11434/api/embed", json={"model": "bge-m3", "input": list_text})
 
